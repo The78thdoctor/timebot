09828d9a15da18cb11f70553e7ec6f958c15ce83
Doctor <45296988+The78thdoctor@users.noreply.github.com>
2019-02-17 20:20:37 -0500

    Update playing.js

--- util/playing.js
+++ util/playing.js
@@ -1,11 +1,11 @@
 module.exports = (bot) => {
-    bot.user.setActivity(`Public Bot!`, { type: "STREAMING", url: "https://www.twitch.tv/elarabots_discord" });
+    bot.user.setActivity(`to The Time Animals`, { type: "LISTENING", url: "https://www.twitch.tv/elarabots_discord" });
     var status = [
-        `Public Discord Bot`
+        `With Time`
     ];
     setInterval(() => {
         let gameval = Math.floor((Math.random() * status.length));
-        bot.user.setActivity(`${status[gameval]}`, { type: "STREAMING", url: "https://www.twitch.tv/elarabots_discord" });
+        bot.user.setActivity(`${status[gameval]}`, { type: "PLAYING", url: "https://www.twitch.tv/elarabots_discord" });
     }, 10 * 1000);
 }
 // This is to set your bot's playing status it will randomize through the statuses
